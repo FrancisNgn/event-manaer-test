61d338567b84c85e9f4e08b94d7fb0296ed84d66
FrancisNg <francisngangue6@gmail.com>
2021-04-14 06:41:02 +0100

    init test project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="app-title">Mes Evenements</div>
+    <div class="app-content">
+      <div class="app-content__event-list">
+        <event-item v-for="event in events" :key="event.id" :event="event"/>
+        rffrfr
+      </div>
+      <div class="app-content__edit-form"><hello-world :msg="currentEventIndex"/></div>
+      <div class="app-content__comment-list">content__comment</div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import {getEvents} from './assets/data.js'
+import EventItem from './components/EventItem.vue'
 
 export default {
   name: 'App',
+  data: ()=> {
+    return {
+      currentEventIndex : 'de',
+      events : [],
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    EventItem
+  },
+  mounted: ()=>{
+    // this.events = [...getEvents()];
+    console.log(getEvents());
   }
 }
 </script>
---
+++ src/components/EventItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="event-item">
+    <h1>{{ event.title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventItem',
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.event-item{
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 12px;
+}
+</style>
